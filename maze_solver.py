ab710c13d5211867636372f9caaa34ab5c29176a
George Buzas <g.buzas@student.rug.nl>
2022-05-02 11:52:43 +0200

    We need to check if the implementation for GREEDY-best first search is correct

--- maze_solver.py
+++ maze_solver.py
@@ -16,6 +16,8 @@ def solve_maze_general(maze, algorithm):
         fr = Fringe("STACK")
     elif algorithm == "UCS":
         fr = Fringe("PRIORITY")
+    elif algorithm == "GREEDY":
+        fr = Fringe("PRIORITY")
     else:
         print("Algorithm not found/implemented, exit")
         return
@@ -44,12 +46,22 @@ def solve_maze_general(maze, algorithm):
         for d in room.get_connections():
             # loop through every possible move
             new_room, cost = room.make_move(d, state.get_cost())        # Get new room after move and cost to get there
+            if algorithm == "GREEDY" and new_room.is_goal():            # We need to check if this implementation of GREEDY is correct
+                new_state = State(new_room, state, cost)
+                # if room is the goal, print that with the statistics and the path and return
+                print("solved")
+                fr.print_stats()
+                new_state.print_path()
+                new_state.print_actions()
+                print()  # print newline
+                maze.print_maze_with_path(new_state)
+                return True
             if new_room.is_visited == False:                            # Only pushing the new room to the fringe, if it has not been visited before
                 new_state = State(new_room, state, cost)                # Create new state with new room and old room
                 fr.push(new_state)                                      # push the new state
                 if algorithm == "BFS" or algorithm == "DFS":
                     new_room.is_visited = True
-        if algorithm == "UCS":
+        if algorithm == "UCS" or algorithm == "GREEDY":
             room.is_visited = True
 
     print("not solved")     # fringe is empty and goal is not found, so maze is not solved
